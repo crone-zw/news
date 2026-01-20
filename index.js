821d97cdd9a9a9e7110711a9b5b1f4e5168a05ca
TCRONEB HACKX <koder2win@gmail.com>
2026-01-20 20:33:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ bot.sendMessage(chatId, `*🤖 ANDROID H4CK — Return of SHINITA*
 _⚔️ Cosmic Warrior Intelligence Bot ⚔️_
 
 ━━━━━━━━━━━━━━━━━━
-👋 *Welcome, ${user}!*
+👋 Welcome, ${user}!
 ━━━━━━━━━━━━━━━━━━
 
 This bot helps you generate smart tracking links to analyze user engagement and technical data.
